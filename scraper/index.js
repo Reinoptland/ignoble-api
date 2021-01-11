a36877b96606cdcf97874ab0e7e8d77da320941d
Reinoptland <reinoptland@gmail.com>
2021-01-11 11:48:41 +0100

    Wrote scraper and parser for the first prize

--- scraper/index.js
+++ scraper/index.js
@@ -26,4 +26,40 @@ async function getHTML() {
   const output = Array.from(uniquePrizeTexts);
   fs.writeFileSync("./prizeCategories.json", JSON.stringify(output));
 }
-getHTML();
+
+async function getFirstPrize() {
+  const response = await axios.get(
+    "https://www.improbable.com/ig-about/winners/"
+  );
+  const dom = new JSDOM(response.data);
+  const strongs = dom.window.document.querySelectorAll("p > strong");
+
+  const firstPrize = strongs[0].parentElement;
+
+  //(\[)([A-Z ,]+)(\])
+
+  // 1. we need strip [ and ] character
+  // 2. split on ', ' -> array countries
+  const countries = firstPrize.textContent.match(/(\[)([A-Z ,]+)(\])/g)[0];
+  var result = countries.substring(1, countries.length - 1).split(", ");
+  console.log(result);
+
+  const reason = firstPrize.textContent.match(/(, for)(.+)/g)[0].substring(6);
+  const formattedReason = reason.charAt(0).toUpperCase() + reason.slice(1);
+  console.log(formattedReason);
+
+  const links = Array.from(firstPrize.querySelectorAll("a"));
+  const resources = links.map((link) => {
+    return { name: link.textContent, url: link.href };
+  });
+
+  fs.writeFileSync(
+    "./acoustics-2020.json",
+    JSON.stringify({
+      countries: result,
+      reason: formattedReason,
+      resources: resources,
+    })
+  );
+}
+getFirstPrize();
