147a1b26a8f027afbd2c5ba0800fc23020adeded
Reinoptland <reinoptland@gmail.com>
2021-01-11 10:52:01 +0100

    Scrape the unique prize categories and write them to a file

--- scraper/index.js
+++ scraper/index.js
@@ -1,5 +1,29 @@
+const fs = require("fs");
 const jsdom = require("jsdom");
+const axios = require("axios");
 const { JSDOM } = jsdom;
 
-const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
-console.log(dom.window.document.querySelector("p").textContent);
+async function getHTML() {
+  console.log("hi");
+  const response = await axios.get(
+    "https://www.improbable.com/ig-about/winners/"
+  );
+  console.log(Object.keys(response));
+  const dom = new JSDOM(response.data);
+  const strongs = dom.window.document.querySelectorAll("p > strong");
+  console.log(Array.from(strongs));
+  const prizeTexts = Array.from(strongs).map((element) => {
+    if (element.innerHTML.includes("PRIZE")) {
+      return element.innerHTML;
+    } else {
+      return element.innerHTML + " PRIZE";
+    }
+  });
+
+  prizeTexts.sort();
+  const uniquePrizeTexts = new Set(prizeTexts);
+  console.log(uniquePrizeTexts);
+  const output = Array.from(uniquePrizeTexts);
+  fs.writeFileSync("./prizeCategories.json", JSON.stringify(output));
+}
+getHTML();
