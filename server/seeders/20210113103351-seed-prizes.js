2809b042cc5310d42c57998e4c88d1fbda2280e3
Reinoptland <reinoptland@gmail.com>
2021-01-19 09:26:34 +0100

    Clean up seeding script

--- server/seeders/20210113103351-seed-prizes.js
+++ server/seeders/20210113103351-seed-prizes.js
@@ -6,19 +6,15 @@ const { Prize, Researcher, Winner } = require("../models");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    let count = 0; // counter for database calls
-    let reasearcherNameSeeded = {}; // researcherNameSeeded['rein'] -> undefined
+    let count = 0;
+    let reasearcherNameSeeded = {};
 
     for (const prize of prizes) {
-      // gather all names of researchers Object -> O(1) { "rein": true }
-      // before we create a prize, check if the winners contain a duplicate
-      // if there is a duplicate, seed in the slow way (with upsert)
-      // if there is no duplicate use seed in 1 go way
       let containsDuplicate = false;
       let winners = [];
       prize.winners.forEach((name) => {
         if (!reasearcherNameSeeded[name]) {
-          reasearcherNameSeeded[name] = true; // { 'rein': true }
+          reasearcherNameSeeded[name] = true;
         } else {
           containsDuplicate = true;
         }
@@ -42,14 +38,16 @@ module.exports = {
           year: parseInt(prize.year),
           description: prize.reason,
         });
+        count++;
 
         let winnerIds = [];
         for (const researcherName of prize.winners) {
-          const storedResearcher = await Researcher.upsert({
-            // find or create the researcher
-            name: researcherName,
+          const [storedResearcher, created] = await Researcher.findOrCreate({
+            where: {
+              name: researcherName,
+            },
           });
-          winnerIds.push(storedResearcher[0].dataValues.id);
+          winnerIds.push(storedResearcher.dataValues.id);
           count++;
         }
 
@@ -62,79 +60,12 @@ module.exports = {
         }
       }
     }
-
-    // amount prizes + amount prizes * winner per prize * 2 database calls
-    // 297 + (297 * 3 * 2)
-    // 297 + (297 * 4 * 2)
-
-    // THE BIG O NOTATION
-    // O(1) -> time constant, no matter how many elements we have FANTASTIC!
-    // for (const prize of prizes) {
-    //   const storedPrize = await Prize.create({
-    //     type: prize.type,
-    //     year: parseInt(prize.year),
-    //     description: prize.reason,
-    //   });
-    //   count++;
-
-    //   let winnerIds = [];
-    //   for (const researcherName of prize.winners) {
-    //     const storedResearcher = await Researcher.upsert({
-    //       // find or create the researcher
-    //       name: researcherName,
-    //     });
-    //     winnerIds.push(storedResearcher[0].dataValues.id);
-    //     count++;
-    //   }
-
-    //   for (const winnerId of winnerIds) {
-    //     const storedWinner = await Winner.create({
-    //       researcherId: winnerId,
-    //       prizeId: storedPrize.dataValues.id,
-    //     });
-    //     count++;
-    //   }
-
-    // console.log(`Perfomed ${count} database actions`);
-    // }
-    // console.log(`Final count: ${count} database actions`);
-    // queryInterFace.bulkInsert(everything) -> if 1 validation does not pass, the whole database call is cancelled
-    // We need the id of prize and a researcher to make to make the association
-    // Can I insert related data with one call?
+    console.log("Number of database calls: ", count);
   },
 
   down: async (queryInterface, Sequelize) => {
-    // time complexity O(1)
     await queryInterface.bulkDelete("Winners", null, {});
     await queryInterface.bulkDelete("Prizes", null, {});
     await queryInterface.bulkDelete("Researchers", null, {});
   },
 };
-
-// let prizeDataOnly = [];
-// let researchers = [];
-// for (const prize of prizes) {
-//   prizeDataOnly = [
-//     ...prizeDataOnly,
-//     {
-//       type: prize.type,
-//       year: parseInt(prize.year),
-//       description: prize.reason,
-//     },
-//   ];
-
-//   researchers = [...researchers, ...prize.winners];
-// }
-
-// const uniqueResearchers = Array.from(new Set(researchers));
-
-// const researcherObjects = uniqueResearchers.map((name) => {
-//   return { name: name };
-// });
-
-// await queryInterface.bulkInsert("Prizes", prizeDataOnly, {});
-// try {
-//   await queryInterface.bulkInsert("Researchers", researcherObjects, {});
-// } catch (error) {
-//   console.log(error);
-// }
