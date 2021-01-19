02aea8fb8868fedf944ddfc343c9ab8d11ec1d93
Reinoptland <reinoptland@gmail.com>
2021-01-19 10:47:15 +0100

    Set env variables for heroku

--- server/config/config.js
+++ server/config/config.js
@@ -18,10 +18,12 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    use_env_variable: "PRODUCTION_DATABASE_URL",
+    dialect: "postgres",
+    pool: {
+      // When seeding parrallel, we are getting max connection errors
+      // So for now we set the max connections to max - 1 (so 5 - 1 -> 4)
+      max: 4,
+    },
   },
 };
