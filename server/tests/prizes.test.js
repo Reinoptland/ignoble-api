9931e10d587758f8a84819fcd0c93935fc5602e3
Reinoptland <reinoptland@gmail.com>
2021-01-20 10:51:22 +0100

    Close connections after running the tests

--- server/tests/prizes.test.js
+++ server/tests/prizes.test.js
@@ -1,10 +1,19 @@
 const app = require("../app");
+const db = require("../models");
 
 const request = require("supertest");
 
-test("should try", () => {
-  const what = request(app).get("/prizes");
-  console.log(what);
+const server = request(app);
 
-  expect(what).toBeDefined();
+afterAll(async () => {
+  await db.sequelize.close();
+});
+
+test("should try", async (done) => {
+  const response = await server.get("/prizes");
+  console.log(response);
+
+  expect(response.status).toBe(200);
+  expect(response.body).toStrictEqual([]);
+  done();
 });
